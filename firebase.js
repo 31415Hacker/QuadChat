4f4cebcd44152faa632f3643261c4d5d63a9a324
31415Hacker <ariqipraditya@gmail.com>
2026-06-01 15:07:04 +0400

    Add Firebase config fallback

--- firebase.js
+++ firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "AIzaSyB22Y06cUUhfSweTGIhHaziHCJTytUkqLI",
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "quadchat-cf697.firebaseapp.com",
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "quadchat-cf697",
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "quadchat-cf697.firebasestorage.app",
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "738776494798",
+  appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:738776494798:web:58454b37922a44fa98969f"
 };
 
 const app = initializeApp(firebaseConfig);
